eb4175d5717c0406944808e8539de8ed28fec303
tkdgusl94 <tkdgusl94@naver.com>
2020-01-06 16:46:13 +0900

    구글 로그인 연동

--- src/main/java/com/leveloper/test/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/leveloper/test/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.leveloper.test.springboot.config.auth.dto;
 import com.leveloper.test.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser { // 인증된 사용자 정보만 필요하다.
+public class SessionUser implements Serializable { // 인증된 사용자 정보만 필요하다.
     private String name;
     private String email;
     private String picture;
